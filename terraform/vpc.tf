c6ff77539221e97d27b8dc64395fe51da926b8a4
Babu Jayaraj <babu.j@hotmail.com>
2026-02-09 16:31:21 +0000

    feat(terraform): add vpc with public subnets

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -7,7 +7,19 @@ resource "aws_vpc" "main" {
   }
 }
 
-# resource "aws_subnet" "public" {
+# resource "aws_subnet" "public" 
+resource "aws_subnet" "public" {
+  for_each = local.public_subnets
 
-# }
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = each.value.cidr_block
+  availability_zone       = each.value.az
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project}-${var.environment}-${each.key}"
+    Tier = "public"
+  }
+}
+ 
 
