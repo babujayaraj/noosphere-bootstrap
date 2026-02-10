0b95ced7d0dafa43d3801f457b3fd0efc95e64d5
Babu J <babuj@alamy.com>
2026-02-10 13:43:54 +0530

    fix(terraform) remove aws_key/secrets

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,16 +3,6 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "aws_access_key" {
-  type    = string
-  default = "test"
-}
-
-variable "aws_secret_key" {
-  type    = string
-  default = "My-VerySecure10x-Secret"
-}
-
 variable "localstack_endpoint" {
   type        = string
   default     = "http://localhost:4566"
