1d061e1295effc57aca7b9f1f3afb134507b798b
Babu J <babuj@alamy.com>
2026-02-10 13:46:29 +0530

    fix(terraform) create lease-privilege role

---
+++ terraform/iam_github_actions.tf
@@ -0,0 +1,121 @@
+variable "github_org" {
+  type        = string
+  description = "GitHub org/user that owns the repo (e.g., babuj)"
+}
+
+variable "github_repo" {
+  type        = string
+  description = "GitHub repo name (e.g., noosphere-bootstrap)"
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+
+  # Common GitHub Actions OIDC thumbprint
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+data "aws_iam_policy_document" "github_oidc_trust" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main",
+        "repo:${var.github_org}/${var.github_repo}:pull_request"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "${var.project}-${var.environment}-gha"
+  assume_role_policy = data.aws_iam_policy_document.github_oidc_trust.json
+}
+
+# Minimal policy for Terraform to manage THIS project's resources.
+# Tighten further with tags if desired.
+data "aws_iam_policy_document" "github_actions_permissions" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:Describe*",
+      "ec2:Create*",
+      "ec2:Delete*",
+      "ec2:Modify*",
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:AuthorizeSecurityGroupEgress",
+      "ec2:RevokeSecurityGroupIngress",
+      "ec2:RevokeSecurityGroupEgress",
+      "ec2:RunInstances",
+      "ec2:TerminateInstances",
+      "ec2:CreateTags",
+      "ec2:DeleteTags"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:CreateBucket",
+      "s3:DeleteBucket",
+      "s3:PutBucketLifecycleConfiguration",
+      "s3:GetBucketLifecycleConfiguration",
+      "s3:PutBucketTagging",
+      "s3:GetBucketTagging",
+      "s3:ListBucket",
+      "s3:GetBucketLocation"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:PutParameter",
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:DeleteParameter",
+      "ssm:DescribeParameters"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = ["sts:GetCallerIdentity"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "github_actions" {
+  name   = "${var.project}-${var.environment}-gha-policy"
+  policy = data.aws_iam_policy_document.github_actions_permissions.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions.arn
+}
+
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions.arn
+}
